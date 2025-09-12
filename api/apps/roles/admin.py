fa890e3f8ab21696e658e8ecdab724b17eafcfe5
Berend Vandenbussche <berend@inuits.eu>
2025-09-12 15:02:07 +0200

    Chore: Restructure collection module

---
+++ api/apps/roles/admin.py
@@ -0,0 +1,40 @@
+ADMIN = {
+    "admin": {
+        "create": {
+            "tenant": {},
+            "user": {},
+            "orchestrator": {},
+            "runner": {},
+            "channel": {},
+            "processor": {},
+            "savedSearch": {}
+        },
+        "read": {
+            "tenant": {},
+            "user": {},
+            "orchestrator": {},
+            "runner": {},
+            "channel": {},
+            "processor": {},
+            "savedSearch": {}
+        },
+        "update": {
+            "tenant": {},
+            "user": {},
+            "orchestrator": {},
+            "runner": {},
+            "channel": {},
+            "processor": {},
+            "savedSearch": {}
+        },
+        "delete": {
+            "tenant": {},
+            "user": {},
+            "orchestrator": {},
+            "runner": {},
+            "channel": {},
+            "processor": {},
+            "savedSearch": {}
+        },
+    },
+}
---
+++ api/apps/roles/user.py
@@ -0,0 +1,40 @@
+USER = {
+    "user": {
+        "create": {
+            "tenant": {},
+            "user": {},
+            "orchestrator": {},
+            "runner": {},
+            "channel": {},
+            "processor": {},
+            "savedSearch": {}
+        },
+        "read": {
+            "tenant": {},
+            "user": {},
+            "orchestrator": {},
+            "runner": {},
+            "channel": {},
+            "processor": {},
+            "savedSearch": {}
+        },
+        "update": {
+            "tenant": {},
+            "user": {},
+            "orchestrator": {},
+            "runner": {},
+            "channel": {},
+            "processor": {},
+            "savedSearch": {}
+        },
+        "delete": {
+            "tenant": {},
+            "user": {},
+            "orchestrator": {},
+            "runner": {},
+            "channel": {},
+            "processor": {},
+            "savedSearch": {}
+        },
+    },
+}
